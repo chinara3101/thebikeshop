c6a4953f18be1386191fbb0cad08f75aa1ad4f13
Chinara1214 <you@example.com>
2017-05-24 19:08:59 +0600

    начинаю работу над домашкой 43

---
+++ app/js/core.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	$('#menu-btn').click(function(){		
+			$('.menu').slideToggle(700);	
+			if ($(this).hasClass('not-active')) {
+				$(this).addClass('is-active').removeClass('not-active');
+			}else{
+				setTimeout(function(){
+					$('.is-active').addClass('not-active').removeClass('is-active')
+				},600)			
+			}		
+		});
+
+	$('.slider').slick({
+    arrows: true,
+    dots: true,
+	  slidesToShow: 1
+	  
+	});
+
+});
